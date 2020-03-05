ad6ffeda05ffeabfe04942d34eac03d282b4b323
Shinji-MCZ <yoshinoya1234@gmail.com>
2020-03-06 02:06:59 +0900

    first commit

--- index.php
+++ index.php
@@ -39,11 +39,59 @@ class Car {
     echo '車の種類:' . $this->name . '、' . '車体番号:' . $this->number . '、' . 'カラー:' . $this->color . 'です。';
   }
 }
+// 以下にCarクラスを継承したTaxiクラスを記述
+class Taxi extends car {
+  private $passenger; 
 
+  private function getPassenger() {
+    return $this->passenger;
+  }
+  private function setPassenger($passenger) {
+    $this->passenger = $passenger;
+  }
 
-// 以下を実行した時にエラーがでないようにして下さい。
+  private function getPickup() {
+    return $this->pickup;
+  }
+  private function setPickup($pickup) {
+    $this->pickup = $pickup;
+  }
+
+  public function lower($passenger) {
+    return $this->lower;
+  }
+
+  public function infomation() {
+    echo '車の車種:' . $this->name . '、' . '車体番号:' . $this->number . '、' . 'カラー:' . $this->color . '、' . '乗車人数は' . $this->lower . 'です。';
+  }
+}
+
+// 以下を実行した時にエラーがでないようにして下さい
 // Carクラスのインスタンスを生成
 // 引数 : 車名、車体番号、カラー
+$car1 = new Car('ランクル', 100, 'black');
+
+// 車の情報を表示
+echo $car1->infomation();
+echo '<br>';
+echo '<hr>';
+
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
+echo '<br>';
+
+// 2人降車
+$taxi1->lower(2);
+echo '<br>';
+
+// 一人降車
+$taxi1->lower(2);
 $car1 = new Car('ランクル', 100, 'black');
 
 // $car1->selfIntrodaction();
